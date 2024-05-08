91b348e387227f92ac65926116ededaef62c6fa2
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2024-05-08 07:22:34 +0200

    Clean up plots imported from *.DATA on project close

--- ApplicationLibCode/ProjectDataModel/VerticalFlowPerformance/RimVfpPlotCollection.h
+++ ApplicationLibCode/ProjectDataModel/VerticalFlowPerformance/RimVfpPlotCollection.h
@@ -39,12 +39,12 @@ public:
 
     void                     addPlot( RimVfpPlot* newPlot ) override;
     std::vector<RimVfpPlot*> plots() const override;
-    void                     deleteChildren();
     RimVfpPlot*              plotForTableNumber( int tableNumber ) const;
 
     size_t plotCount() const final;
     void   insertPlot( RimVfpPlot* vfpPlot, size_t index ) final;
     void   removePlot( RimVfpPlot* vfpPlot ) final;
+    void   deleteAllPlots() override;
 
     RimVfpDeck* addDeck( const QString& filename );
 
