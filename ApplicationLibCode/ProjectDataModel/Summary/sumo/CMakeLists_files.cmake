f89b14e93833fe4a764185c95957a3824ffd2e34
magnesj <1793152+magnesj@users.noreply.github.com>
2024-06-28 08:14:37 +0000

    Fixes by cmake-format

--- ApplicationLibCode/ProjectDataModel/Summary/sumo/CMakeLists_files.cmake
+++ ApplicationLibCode/ProjectDataModel/Summary/sumo/CMakeLists_files.cmake
@@ -1,6 +1,5 @@
-set(SOURCE_GROUP_HEADER_FILES
-    ${CMAKE_CURRENT_LIST_DIR}/RimSummaryEnsembleSumo.h
-    ${CMAKE_CURRENT_LIST_DIR}/RimSummaryCaseSumo.h
+set(SOURCE_GROUP_HEADER_FILES ${CMAKE_CURRENT_LIST_DIR}/RimSummaryEnsembleSumo.h
+                              ${CMAKE_CURRENT_LIST_DIR}/RimSummaryCaseSumo.h
 )
 
 set(SOURCE_GROUP_SOURCE_FILES
@@ -11,7 +10,6 @@ set(SOURCE_GROUP_SOURCE_FILES
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
 
 list(APPEND CODE_SOURCE_FILES ${SOURCE_GROUP_SOURCE_FILES})
-
 
 source_group(
   "ProjectDataModel\\Summary"
