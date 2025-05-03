973343e380efe73997b610436af92cf94a67d30d
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-05-03 09:16:36 +0200

    Add ensemble file set to project

---
+++ ApplicationLibCode/ProjectDataModel/EnsembleFileset/RimEnsembleFilesetCollection.cpp
@@ -0,0 +1,116 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2025 Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RimEnsembleFilesetCollection.h"
+#include "RimEnsembleFileset.h"
+
+#include "cafCmdFeatureMenuBuilder.h"
+#include "cafPdmUiComboBoxEditor.h"
+#include "cafPdmUiLineEditor.h"
+
+CAF_PDM_SOURCE_INIT( RimEnsembleFilesetCollection, "EnsembleFilesetCollection" );
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+RimEnsembleFilesetCollection::RimEnsembleFilesetCollection()
+{
+    CAF_PDM_InitObject( "Ensemble Fileset Collection", ":/Cases16x16.png", "", "" );
+
+    CAF_PDM_InitFieldNoDefault( &m_filesets, "Filesets", "Filesets", "", "", "" );
+    m_filesets.uiCapability()->setUiHidden( true );
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimEnsembleFilesetCollection::addFileset( RimEnsembleFileset* fileset )
+{
+    if ( fileset )
+    {
+        m_filesets.push_back( fileset );
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimEnsembleFilesetCollection::removeFileset( RimEnsembleFileset* fileset )
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+std::vector<RimEnsembleFileset*> RimEnsembleFilesetCollection::filesets() const
+{
+    return m_filesets.childrenByType();
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+QList<caf::PdmOptionItemInfo> RimEnsembleFilesetCollection::ensembleFileSetOptions() const
+{
+    QList<caf::PdmOptionItemInfo> options;
+    for ( const auto& fileset : m_filesets )
+    {
+        options.push_back( caf::PdmOptionItemInfo( fileset->name(), fileset, false, fileset->uiIconProvider() ) );
+    }
+    return options;
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimEnsembleFilesetCollection::updateConnectedEditors()
+{
+    for ( const auto& fileset : m_filesets )
+    {
+        fileset->updateConnectedEditors();
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimEnsembleFilesetCollection::defineUiOrdering( QString uiConfigName, caf::PdmUiOrdering& uiOrdering )
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimEnsembleFilesetCollection::defineUiTreeOrdering( caf::PdmUiTreeOrdering& uiTreeOrdering, QString uiConfigName )
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimEnsembleFilesetCollection::fieldChangedByUi( const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue )
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RimEnsembleFilesetCollection::appendMenuItems( caf::CmdFeatureMenuBuilder& menuBuilder ) const
+{
+    menuBuilder << "RicImportEnsembleFilesetFeature";
+}
