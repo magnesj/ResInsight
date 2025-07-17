70ea00851b921f93131fe8fb889abce9c802f80e
magnesj <1793152+magnesj@users.noreply.github.com>
2025-07-17 08:59:53 +0000

    Fixes by clang-format

--- ApplicationLibCode/Application/Tools/RiaStdStringTools.cpp
+++ ApplicationLibCode/Application/Tools/RiaStdStringTools.cpp
@@ -239,7 +239,7 @@ std::pair<std::string_view, std::string_view> RiaStdStringTools::splitAtWhitespa
     const char* pos  = data;
 
     // Find separator, which is the first whitespace character
-    while ( pos < end && str.find_first_of( WHITESPACE, pos - data ) != static_cast<std::string_view::size_type>(pos - data ))
+    while ( pos < end && str.find_first_of( WHITESPACE, pos - data ) != static_cast<std::string_view::size_type>( pos - data ) )
         ++pos;
 
     if ( pos >= end ) return { str, {} };
@@ -248,7 +248,7 @@ std::pair<std::string_view, std::string_view> RiaStdStringTools::splitAtWhitespa
 
     // Skip all whitespace characters
     size_t offset = str.find_first_not_of( WHITESPACE, pos - data );
-    pos = (offset == std::string::npos) ? end : data + offset;
+    pos           = ( offset == std::string::npos ) ? end : data + offset;
 
     if ( pos >= end ) return { first, {} };
 
