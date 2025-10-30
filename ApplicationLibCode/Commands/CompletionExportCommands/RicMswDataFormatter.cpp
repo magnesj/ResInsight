79e5602a451c54ebaa46e64b082e04532f87f17a
magnesj <1793152+magnesj@users.noreply.github.com>
2025-10-30 05:58:49 +0000

    Fixes by clang-format

--- ApplicationLibCode/Commands/CompletionExportCommands/RicMswDataFormatter.cpp
+++ ApplicationLibCode/Commands/CompletionExportCommands/RicMswDataFormatter.cpp
@@ -42,7 +42,7 @@ void formatWelsegsRows( RifTextDataTableFormatter& formatter, const RowContainer
         formatter.addOptionalValue( row.depth, RicMswExportInfo::defaultDoubleValue() );
         formatter.addOptionalValue( row.diameter, RicMswExportInfo::defaultDoubleValue() );
         formatter.addOptionalValue( row.roughness, RicMswExportInfo::defaultDoubleValue() );
-        
+
         formatter.addOptionalComment( row.description );
         formatter.rowCompleted();
     }
