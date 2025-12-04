ef12ba184ed55072db01226360438ff117f1696c
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-12-04 14:53:02 +0100

    Rename RigSoilResultCalculator to RigSaturationResultCalculator
    - Rename files from RigSoilResultCalculator.* to RigSaturationResultCalculator.*
    - Update all include statements and references
    - Update CMakeLists_files.cmake to reference new filenames
    - Update comment in RifReaderOpmRft.cpp for consistency
    - Class name reflects broader scope: handles SOIL, SWAT, and SGAS calculations
    - Build verification confirms successful renaming with no errors

--- ApplicationLibCode/ReservoirDataModel/ResultCalculators/CMakeLists_files.cmake
+++ ApplicationLibCode/ReservoirDataModel/ResultCalculators/CMakeLists_files.cmake
@@ -1,6 +1,6 @@
 set(SOURCE_GROUP_HEADER_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RigEclipseResultCalculator.h
-    ${CMAKE_CURRENT_LIST_DIR}/RigSoilResultCalculator.h
+    ${CMAKE_CURRENT_LIST_DIR}/RigSaturationResultCalculator.h
     ${CMAKE_CURRENT_LIST_DIR}/RigFaultDistanceResultCalculator.h
     ${CMAKE_CURRENT_LIST_DIR}/RigMobilePoreVolumeResultCalculator.h
     ${CMAKE_CURRENT_LIST_DIR}/RigIndexIjkResultCalculator.h
@@ -13,7 +13,7 @@ set(SOURCE_GROUP_HEADER_FILES
 
 set(SOURCE_GROUP_SOURCE_FILES
     ${CMAKE_CURRENT_LIST_DIR}/RigEclipseResultCalculator.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/RigSoilResultCalculator.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/RigSaturationResultCalculator.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RigFaultDistanceResultCalculator.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RigMobilePoreVolumeResultCalculator.cpp
     ${CMAKE_CURRENT_LIST_DIR}/RigIndexIjkResultCalculator.cpp
