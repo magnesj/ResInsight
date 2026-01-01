4d38d58e9d59a60e1c749109683537aa253b7654
magnesj <1793152+magnesj@users.noreply.github.com>
2026-01-01 08:46:11 +0000

    Fixes by clang-format

--- ApplicationLibCode/Commands/RicUserDefinedCalculatorUi.h
+++ ApplicationLibCode/Commands/RicUserDefinedCalculatorUi.h
@@ -51,8 +51,8 @@ public:
     virtual void                                 notifyCalculatedNameChanged( int id, const QString& newName ) const = 0;
 
 protected:
-    void                          defineUiOrdering( QString uiConfigName, caf::PdmUiOrdering& uiOrdering ) override;
-    void                          fieldChangedByUi( const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue ) override;
+    void defineUiOrdering( QString uiConfigName, caf::PdmUiOrdering& uiOrdering ) override;
+    void fieldChangedByUi( const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue ) override;
     QList<caf::PdmOptionItemInfo> calculateValueOptions( const caf::PdmFieldHandle* fieldNeedingOptions ) override;
     void                          onEditorWidgetsCreated() override;
 
