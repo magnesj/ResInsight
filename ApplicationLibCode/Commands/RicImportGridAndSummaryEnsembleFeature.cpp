4f27d2b2a1013b3c1bb5eac3cb5635d016fd5261
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2026-03-04 14:36:35 +0100

    RicImportGridAndSummaryEnsembleFeature: Add command that invokes combined import dialog
    Make importSingleEnsembleFileSet public in RicImportEnsembleFeature to allow external callers.

---
+++ ApplicationLibCode/Commands/RicImportGridAndSummaryEnsembleFeature.cpp
@@ -0,0 +1,83 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2026-     Equinor ASA
+//
+//  ResInsight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ResInsight is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+//  for more details.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "RicImportGridAndSummaryEnsembleFeature.h"
+
+#include "RiaEnsembleNameTools.h"
+#include "RiaGuiApplication.h"
+#include "Summary/RiaSummaryDefines.h"
+
+#include "EclipseCommands/RicCreateGridCaseEnsemblesFromFilesFeature.h"
+#include "RicImportEnsembleFeature.h"
+#include "RicImportGridAndSummaryEnsembleDialog.h"
+
+#include <QAction>
+#include <QIcon>
+
+CAF_CMD_SOURCE_INIT( RicImportGridAndSummaryEnsembleFeature, "RicImportGridAndSummaryEnsembleFeature" );
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RicImportGridAndSummaryEnsembleFeature::onActionTriggered( bool isChecked )
+{
+    auto result = RicImportGridAndSummaryEnsembleDialog::runDialog( RiaGuiApplication::widgetToUseAsParent() );
+
+    if ( !result.ok ) return;
+
+    if ( result.createGridEnsemble && !result.gridFiles.isEmpty() )
+    {
+        if ( result.groupingMode == RiaDefines::EnsembleGroupingMode::NONE )
+        {
+            RicCreateGridCaseEnsemblesFromFilesFeature::importSingleGridCaseEnsemble( result.gridFiles );
+        }
+        else
+        {
+            auto groups = RiaEnsembleNameTools::groupFilesByEnsemble( result.gridFiles, result.groupingMode );
+            for ( const auto& group : groups )
+            {
+                RicCreateGridCaseEnsemblesFromFilesFeature::importSingleGridCaseEnsemble( group );
+            }
+        }
+    }
+
+    if ( result.createSummaryEnsemble && !result.summaryFiles.isEmpty() )
+    {
+        if ( result.groupingMode == RiaDefines::EnsembleGroupingMode::NONE )
+        {
+            RicImportEnsembleFeature::importSingleEnsembleFileSet( result.summaryFiles, false, result.groupingMode, RiaDefines::FileType::SMSPEC );
+        }
+        else
+        {
+            auto groupedFiles = RiaEnsembleNameTools::groupFilesByEnsembleName( result.summaryFiles, result.groupingMode );
+            for ( const auto& [groupName, fileNames] : groupedFiles )
+            {
+                RicImportEnsembleFeature::importSingleEnsembleFileSet( fileNames, false, result.groupingMode, RiaDefines::FileType::SMSPEC, groupName );
+            }
+        }
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
+void RicImportGridAndSummaryEnsembleFeature::setupActionLook( QAction* actionToSetup )
+{
+    actionToSetup->setText( "Import Grid and Summary Ensemble" );
+    actionToSetup->setIcon( QIcon( ":/CreateGridCaseGroup16x16.png" ) );
+}
