e36df6c059c90cc36652b1287a57e93b84127fd5
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2026-05-02 16:44:36 +0200

    AppEnum: Add file header and function headers to cafAppEnum.cpp

--- Fwk/AppFwk/cafProjectDataModel/cafPdmCore/cafAppEnum.cpp
+++ Fwk/AppFwk/cafProjectDataModel/cafPdmCore/cafAppEnum.cpp
@@ -1,6 +1,43 @@
-// Out-of-line implementations of AppEnumMapperBase. By keeping the loops, lookups, and QString
-// machinery here in one TU instead of in the AppEnum<T> class template, every method is compiled
-// once instead of once per enum type.
+//##################################################################################################
+//
+//   Custom Visualization Core library
+//   Copyright (C) 2011-2013 Ceetron AS
+//
+//   This library may be used under the terms of either the GNU General Public License or
+//   the GNU Lesser General Public License as follows:
+//
+//   GNU General Public License Usage
+//   This library is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//   FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   See the GNU General Public License at <<http://www.gnu.org/licenses/gpl.html>>
+//   for more details.
+//
+//   GNU Lesser General Public License Usage
+//   This library is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation; either version 2.1 of the License, or
+//   (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//   FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   See the GNU Lesser General Public License at <<http://www.gnu.org/licenses/lgpl-2.1.html>>
+//   for more details.
+//
+//##################################################################################################
+
+// AppEnumMapperBase holds the type-erased mapping (int <-> text/uiText/aliases) shared by every
+// AppEnum<T> instantiation. Defining its members in this translation unit instead of inline in the
+// class template means each lookup and QString helper is compiled once for the whole program rather
+// than once per enum type, which keeps build times and binary size in check.
 
 #include "cafAppEnum.h"
 
@@ -9,11 +46,17 @@
 namespace caf
 {
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 bool AppEnumMapperBase::EnumData::isMatching( const QString& text ) const
 {
     return text == m_text || m_aliases.contains( text );
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 void AppEnumMapperBase::addItem( int enumVal, const QString& text, QString uiText, const QStringList& aliases )
 {
     // Make sure the alias text is unique for enum
@@ -30,12 +73,18 @@ void AppEnumMapperBase::addItem( int enumVal, const QString& text, QString uiTex
     m_mapping.push_back( { enumVal, text.trimmed(), uiText, aliases } );
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 void AppEnumMapperBase::setDefault( int defaultEnumValue )
 {
     m_defaultValue      = defaultEnumValue;
     m_defaultValueIsSet = true;
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 int AppEnumMapperBase::defaultValue() const
 {
     if ( m_defaultValueIsSet ) return m_defaultValue;
@@ -43,6 +92,9 @@ int AppEnumMapperBase::defaultValue() const
     return m_mapping[0].m_enumVal;
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 bool AppEnumMapperBase::isValid( const QString& text ) const
 {
     for ( size_t idx = 0; idx < m_mapping.size(); ++idx )
@@ -52,11 +104,17 @@ bool AppEnumMapperBase::isValid( const QString& text ) const
     return false;
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 size_t AppEnumMapperBase::size() const
 {
     return m_mapping.size();
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 bool AppEnumMapperBase::enumVal( int& value, const QString& text ) const
 {
     value = defaultValue();
@@ -74,6 +132,9 @@ bool AppEnumMapperBase::enumVal( int& value, const QString& text ) const
     return false;
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 bool AppEnumMapperBase::enumVal( int& value, size_t index ) const
 {
     value = defaultValue();
@@ -85,6 +146,9 @@ bool AppEnumMapperBase::enumVal( int& value, size_t index ) const
     return false;
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 size_t AppEnumMapperBase::index( int enumValue ) const
 {
     size_t idx;
@@ -95,6 +159,9 @@ size_t AppEnumMapperBase::index( int enumValue ) const
     return idx; // returns size() if not found, matching prior behavior
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 QString AppEnumMapperBase::uiText( int value ) const
 {
     for ( size_t idx = 0; idx < m_mapping.size(); ++idx )
@@ -104,6 +171,9 @@ QString AppEnumMapperBase::uiText( int value ) const
     return "";
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 QStringList AppEnumMapperBase::uiTexts() const
 {
     QStringList uiTextList;
@@ -114,6 +184,9 @@ QStringList AppEnumMapperBase::uiTexts() const
     return uiTextList;
 }
 
+//--------------------------------------------------------------------------------------------------
+///
+//--------------------------------------------------------------------------------------------------
 QString AppEnumMapperBase::text( int value ) const
 {
     for ( size_t idx = 0; idx < m_mapping.size(); ++idx )
