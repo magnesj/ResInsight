a95383b0a1c35dac4ca471b6d6814aea68e89fb0
magnesj <1793152+magnesj@users.noreply.github.com>
2026-06-01 13:34:18 +0000

    Fixes by cmake-format

--- ApplicationLibCode/Automation/CMakeLists_files.cmake
+++ ApplicationLibCode/Automation/CMakeLists_files.cmake
@@ -1,11 +1,9 @@
-set(SOURCE_GROUP_HEADER_FILES
-    ${CMAKE_CURRENT_LIST_DIR}/RiaAutomationServer.h
-    ${CMAKE_CURRENT_LIST_DIR}/RiaAutomationJson.h
+set(SOURCE_GROUP_HEADER_FILES ${CMAKE_CURRENT_LIST_DIR}/RiaAutomationServer.h
+                              ${CMAKE_CURRENT_LIST_DIR}/RiaAutomationJson.h
 )
 
-set(SOURCE_GROUP_SOURCE_FILES
-    ${CMAKE_CURRENT_LIST_DIR}/RiaAutomationServer.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/RiaAutomationJson.cpp
+set(SOURCE_GROUP_SOURCE_FILES ${CMAKE_CURRENT_LIST_DIR}/RiaAutomationServer.cpp
+                              ${CMAKE_CURRENT_LIST_DIR}/RiaAutomationJson.cpp
 )
 
 list(APPEND CODE_HEADER_FILES ${SOURCE_GROUP_HEADER_FILES})
